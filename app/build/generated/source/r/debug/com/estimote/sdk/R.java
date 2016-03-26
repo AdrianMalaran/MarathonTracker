9825ec29f35445fad7913d76556dab8fc4e256fb
syuyitung <samyuyitung@gmail.com>
2016-03-26 10:46:09 -0400

    added the map

--- app/build/generated/source/r/debug/com/estimote/sdk/R.java
+++ app/build/generated/source/r/debug/com/estimote/sdk/R.java
@@ -8,17 +8,17 @@ package com.estimote.sdk;
 
 public final class R {
 	public static final class string {
-		public static final int error_no_bluetooth_enabled = 0x7f060017;
-		public static final int error_no_bluetooth_le = 0x7f060018;
-		public static final int error_no_location_permission = 0x7f060019;
-		public static final int requesting_location_access = 0x7f06001a;
-		public static final int requesting_location_access_cancel = 0x7f06001b;
-		public static final int requesting_location_access_ok = 0x7f06001c;
-		public static final int requesting_location_access_rationale = 0x7f06001d;
-		public static final int requesting_location_permission = 0x7f06001e;
-		public static final int requesting_location_permission_rationale = 0x7f06001f;
+		public static final int error_no_bluetooth_enabled = 0x7f07004b;
+		public static final int error_no_bluetooth_le = 0x7f07004c;
+		public static final int error_no_location_permission = 0x7f07004d;
+		public static final int requesting_location_access = 0x7f070050;
+		public static final int requesting_location_access_cancel = 0x7f070051;
+		public static final int requesting_location_access_ok = 0x7f070052;
+		public static final int requesting_location_access_rationale = 0x7f070053;
+		public static final int requesting_location_permission = 0x7f070054;
+		public static final int requesting_location_permission_rationale = 0x7f070055;
 	}
 	public static final class style {
-		public static final int Theme_Transparent = 0x7f0900f4;
+		public static final int Theme_Transparent = 0x7f0a00f9;
 	}
 }
