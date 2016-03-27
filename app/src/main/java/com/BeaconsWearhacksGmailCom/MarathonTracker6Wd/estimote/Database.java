685885272562c582765887eb4dc6c0e11fc964d0
syuyitung <samyuyitung@gmail.com>
2016-03-26 20:51:43 -0400

    pre-pull commit

--- app/src/main/java/com/BeaconsWearhacksGmailCom/MarathonTracker6Wd/estimote/Database.java
+++ app/src/main/java/com/BeaconsWearhacksGmailCom/MarathonTracker6Wd/estimote/Database.java
@@ -1,10 +1,10 @@
 package com.BeaconsWearhacksGmailCom.MarathonTracker6Wd.estimote;
 
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+        import android.content.ContentValues;
+        import android.content.Context;
+        import android.database.Cursor;
+        import android.database.sqlite.SQLiteDatabase;
+        import android.database.sqlite.SQLiteOpenHelper;
 
 /**
  * Created by swagpanduhbur on 3/26/2016.
@@ -12,23 +12,52 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class Database extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "Tracker.db";
     public static final String TABLE_NAME = "runner_tracker";
-    public static final String COL_1 = "DISTANCE_TRAVELLED";
-    public static final String COL_2 = "CALORIES_BURNED";
-    public static final String COL_3 = "STEP_COUNT";
-    public static final String COL_4 = "MAX_SPEED";
-    public static final String COL_5 = "TOTAL_TIME_TAKEN";
-    public static final String COL_6 = "SECTION";
+    public static final String COL_1 = "RACE_NAME";
+    public static final String COL_2 = "DISTANCE_TRAVELLED";
+    public static final String COL_3 = "CALORIES_BURNED";
+    public static final String COL_4 = "STEP_COUNT";
+    public static final String COL_5 = "MAX_SPEED";
+    public static final String COL_6 = "TOTAL_TIME_TAKEN";
+    public static final String COL_7 = "SECTION";
 
+    //BEACON TABLE
+    public static final String TABLE_NAME1 = "beacon_info";
+    public static final String COL1_1 = "ID";
+    public static final String COL1_2 = "MILE_MARKER";
+    public static final String COL1_3 = "LONGITUDE";
+    public static final String COL1_4 = "LATITUDE";
+    public static final String COL1_5 = "TYPE";
+
+    //Historical Table
+    public static final String TABLE_NAME2 = "historical_data";
+    public static final String COL2_1 = "RACE_NAME";
+    public static final String COL2_2 = "DISTANCE_TRAVELLED";
+    public static final String COL2_3 = "CALORIES_BURNED";
+    public static final String COL2_4 = "STEP_COUNT";
+    public static final String COL2_5 = "MAX_SPEED";
+    public static final String COL2_6 = "SPLIT_TIME";
+
+    SQLiteDatabase db;
 
     public Database(Context context){
         super(context, DATABASE_NAME, null, 1);
 
     }
 
-    @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create table " + TABLE_NAME + " (DISTANCE_TRAVELLED DOUBLE, CALORIES_BURNED DOUBLE, STEP_COUNT INTEGER, MAX_SPEED DOUBLE, TOTAL_TIME_TAKEN DOUBLE, SECTION INT PRIMARY KEY AUTOINCREMENT)");
+        db.execSQL("create table " + TABLE_NAME2 + " (DISTANCE_TRAVELLED DOUBLE, CALORIES_BURNED DOUBLE, STEP_COUNT INTEGER, MAX_SPEED DOUBLE, TOTAL_TIME_TAKEN DOUBLE, SECTION INT PRIMARY KEY AUTOINCREMENT)");
     }
+
+    public void createHistoric() {
+        db.execSQL("create table " + TABLE_NAME2 + " (DISTANCE_TRAVELLED DOUBLE, CALORIES_BURNED DOUBLE, STEP_COUNT INTEGER, MAX_SPEED DOUBLE, TOTAL_TIME_TAKEN DOUBLE, SECTION INT PRIMARY KEY AUTOINCREMENT)");
+    }
+
+    public void createLocal() {
+        db.execSQL("create table " + TABLE_NAME + " (DISTANCE_TRAVELLED DOUBLE, CALORIES_BURNED DOUBLE, STEP_COUNT INTEGER, MAX_SPEED DOUBLE, TOTAL_TIME_TAKEN DOUBLE, SECTION INT PRIMARY KEY AUTOINCREMENT)");
+    }
+
+
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
@@ -57,6 +86,10 @@ public class Database extends SQLiteOpenHelper {
         Cursor res = db.rawQuery("select * from " +  TABLE_NAME, null);
         return res;
     }
+
+
+
+
 
     public boolean updateData(String distanceTravelled, String caloriesBurned, String stepCount, String maxSpeed, String timeTaken, String section){
         SQLiteDatabase db = this.getWritableDatabase();
